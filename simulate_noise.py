ed99137e2b47f1725813b3bde20c17d73c64d882
Lennart Frahm <l.frahm@fz-juelich.de>
2021-02-01 21:48:14 +0100

    fix clustering. simplified p-value calculation. change tfce calculation to parallelized version.

--- simulate_noise.py
+++ simulate_noise.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy import ndimage
-from tfce import tfce
+from tfce import tfce_par
 from scipy.stats import norm
-import timeit
+from joblib import Parallel, delayed
+from mytime import tic, toc
 
 def simulate_noise(sample_space,
                    s0,
@@ -12,16 +13,17 @@ def simulate_noise(sample_space,
                    eps=np.finfo(float).eps,
                    uc = 0.001,
                    tfce_params = [0.1, 0.6, 2],
+                   template_shape = [91,109,91],
                    pad_tmp_shape=[121, 139, 121],
                    voxel_dims=[2,2,2],
                    step=10000):
-    starttime = timeit.default_timer()
+    
     sample_space_size = sample_space.shape[1]
     vx = np.zeros((len(s0), sample_space_size))
-    data = np.zeros((pad_tmp_shape))
 
     for i, s in enumerate(s0):
         sample_peaks = sample_space[:,np.random.randint(0,sample_space_size, num_peaks[s])]
+        data = np.zeros((pad_tmp_shape))
         for peak in sample_peaks.T:
             x_range = (peak[0],peak[0]+31)
             y_range = (peak[1],peak[1]+31)
@@ -29,35 +31,35 @@ def simulate_noise(sample_space,
             data[x_range[0]:x_range[1], y_range[0]:y_range[1], z_range[0]:z_range[1]] = \
                 np.maximum(data[x_range[0]:x_range[1], y_range[0]:y_range[1], z_range[0]:z_range[1]],
                            kernels[s])
+        vx[i,:] = data[sample_space[0]+15, sample_space[1]+15, sample_space[2]+15]
 
-        vx[i,:] = data[sample_space[0], sample_space[1], sample_space[2]]
-    
     data = 1-np.prod(1-vx, axis=0)
 
     # Peak ALE threshold
     nm = np.max(data)
-    print("time taken for ale calc:" + str(timeit.default_timer() - starttime))
-    starttime = timeit.default_timer()
     # Cluster level threshold
-    palette, index = np.unique(np.round(data*step), return_inverse=True)
-    index = palette[index].astype(int)
-    p = c_null[index].reshape(data.shape)
+    ale_step = np.round(data*step).astype(int)
+    p = np.array([c_null[i] for i in ale_step])
     p[p < eps] = eps
-    labels, cluster_count = ndimage.label(np.array(p < uc))
-    nn = np.max(np.bincount(labels[labels>0]))
 
+    sig_coords = sample_space[:,p < uc]
+    sig_arr = np.zeros(template_shape)
+    sig_arr[tuple(sig_coords)] = 1
+    labels, cluster_count = ndimage.label(sig_arr)
+    if cluster_count == 1:
+        nn = 0
+    else:
+        nn = np.max(np.bincount(labels[labels>0]))
+        
     z = np.zeros(pad_tmp_shape)
     z[sample_space[0], sample_space[1], sample_space[2]] = norm.ppf(1-p)
     z = z[15:z.shape[0]-15,15:z.shape[1]-15, 15:z.shape[2]-15]
-    print("time taken for p-value calc:" + str(timeit.default_timer() - starttime))
-    starttime = timeit.default_timer()
-    # TFCE threshold
-    tfce_arr = tfce(invol=z,
-                    voxel_dims=voxel_dims,
-                    dh=tfce_params[0],
-                    E=tfce_params[1],
-                    H=tfce_params[2])
-    nt = np.max(tfce_arr)
-    print("time taken for tfce calc:" + str(timeit.default_timer() - starttime))
     
+    # TFCE threshold
+    tfce_arr = np.zeros(z.shape)
+    dh, H, E = tfce_params
+    vals, masks = zip(*Parallel(n_jobs=-1, backend="threading")(delayed(tfce_par)(invol=z, h=h, dh=dh) for h in np.arange(0, np.max(z), dh)))
+    for i in range(len(vals)):
+        tfce_arr[masks[i]] += vals[i]
+    nt = np.max(tfce_arr)
     return nm, nn, nt
